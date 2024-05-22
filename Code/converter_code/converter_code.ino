5d5a186cb49e29fca98889dd1ab2f4142f6a3881
Trif Robert <robert.trif@student.upt.ro>
2024-05-23 01:39:38 +0300

    Connection with actuators.

--- Code/converter_code/converter_code.ino
+++ Code/converter_code/converter_code.ino
@@ -2,18 +2,26 @@
 #include <Adafruit_INA219.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <driver/adc.h> 
 
-const int NUM_MODULES = 1;  //number of INA219 Modules
+Adafruit_INA219 ina_219;
 
-const int vIn = 32;   //corresponds to GPIO35
-const int PWM = 33;   //corresponds to GPIO32
-const int temperature_s1 = 25; //pin for the first temperature sensor; corresponds to GPIO32
-const int temperature_s2 = 26; //pin for the second temperature sensor; corresponds to GPIO32
+#define temperature_s1 34 //pin for the first temperature sensor; corresponds to GPI36
+#define temperature_s2 35 //pin for the second temperature sensor; corresponds to GPIO39
 
-int pwm = 256;
-const int pwm_channel = 0;
-const int freqency = 10000;   //Hz
+const int PWM = 23;   //corresponds to GPIO23
+const int pwm_channel = 6;
+const int freqency = 30000;   //Hz
 const int resolution = 8;
+int pwm = 255;
+
+int motorspeed_l = 33;
+int leftMotors_pin1 = 25;
+int leftMotors_pin2 = 26;
+
+int rightMotors_pin1 = 27;
+int rightMotors_pin2 = 14;
+int motorspeed_r = 12;
 
 //access for Dallas temperature sensors
 OneWire oneWire_s1(temperature_s1);
@@ -21,15 +29,14 @@ OneWire oneWire_s2(temperature_s2);
 DallasTemperature sensor1(&oneWire_s1);
 DallasTemperature sensor2(&oneWire_s2);
 
-Adafruit_INA219 modules[NUM_MODULES] = {
-  Adafruit_INA219(0x40)
-  // Adafruit_INA219(0x41)
-};
+const float divider_ratio = 0.2985;   // Voltage divider ratio based on resistences value: 10 / (23.5 + 10)
 
-float get_input_voltage(int n_samples, float peak_v){
+float get_input_voltage(int n_samples, float ref_v){
   float voltage = 0.0;
+  
   for(int i=0; i < n_samples; i++){
-    voltage += analogRead(vIn) * peak_v / 4095.0;   
+    int raw_adc = adc1_get_raw(ADC1_CHANNEL_0);  // Read the raw ADC value from channel 3, GPIO39
+    voltage += (raw_adc * ref_v / 4095.0) / divider_ratio;  
   }
   voltage = voltage/n_samples;
   return(voltage);
@@ -37,9 +44,20 @@ float get_input_voltage(int n_samples, float peak_v){
 
 void setup() {
 
+  pinMode(leftMotors_pin1, OUTPUT);
+  pinMode(leftMotors_pin2, OUTPUT);
+  pinMode(rightMotors_pin1, OUTPUT);
+  pinMode(rightMotors_pin2, OUTPUT);
+
+  pinMode(motorspeed_l, OUTPUT);  //controls the left motor speed - range(0, 255)
+  pinMode(motorspeed_r, OUTPUT);  //controls the right motor speed - range(0, 255)
+
+  adc1_config_width(ADC_WIDTH_BIT_12);  // Set ADC resolution to 12 bits
+  adc1_config_channel_atten(ADC1_CHANNEL_0, ADC_ATTEN_DB_11);  // Set attenuation to 11dB for 0-3.6V range
+
   ledcSetup(pwm_channel, freqency, resolution);
   ledcAttachPin(PWM, pwm_channel);
-  
+
   Serial.begin(115200);
   sensor1.begin();
   sensor2.begin();
@@ -49,25 +67,23 @@ void setup() {
       delay(1000);
   }
 
-  for (int i = 0; i < NUM_MODULES; i++) {
-    if (!modules[i].begin()) {
-      Serial.print("Failed to find module "); Serial.println(i);
-      while (1) {
-        Serial.println("Stuck here!");
-        delay(1000);
-      }
+  if (!ina_219.begin()) {
+    Serial.print("Failed to find module ina_219!");
+    while (1) {
+      Serial.println("Stuck here!");
+      delay(1000);
     }
-    modules[i].setCalibration_32V_2A();
   }
+  ina_219.setCalibration_32V_2A();
   Serial.println("Measuring voltage and current with the two INA219 moudules...");
 }
 
 void loop() {  
 
-  float expected_value = 10;
-  float input = get_input_voltage(10, 10);
-  float iBatt = modules[0].getCurrent_mA();
-  float vBatt = modules[0].getBusVoltage_V() + (modules[0].getShuntVoltage_mV() / 1000);
+  float expected_value = 2;
+  float input = get_input_voltage(10, 3.3);
+  float iBatt = ina_219.getCurrent_mA();
+  float vBatt = ina_219.getBusVoltage_V() + (ina_219.getShuntVoltage_mV() / 1000);
 
   sensor1.requestTemperatures();
   sensor2.requestTemperatures();
@@ -86,14 +102,23 @@ void loop() {
 
   ledcWrite(pwm_channel,pwm);
 
+  // digitalWrite(leftMotors_pin1, LOW);
+  // digitalWrite(leftMotors_pin2, HIGH);
+
+  // digitalWrite(rightMotors_pin1, HIGH);
+  // digitalWrite(rightMotors_pin2, LOW);
+
+  // digitalWrite(motorspeed_l, HIGH);
+  // digitalWrite(motorspeed_r, HIGH);
+
   Serial.print("PWM Value: ");
   Serial.println(pwm);
-  Serial.print("Input voltage:       "); Serial.print(input); Serial.println(" V");
-  Serial.print("Battery Voltage:     "); Serial.print(vBatt); Serial.println(" V");
-  Serial.print("Charging Current:    "); Serial.print(iBatt); Serial.println(" mA");
+  Serial.print("Input voltage:         "); Serial.print(input); Serial.println(" V");
+  Serial.print("Battery Voltage:       "); Serial.print(vBatt); Serial.println(" V");
+  Serial.print("Charging Current:      "); Serial.print(iBatt); Serial.println(" mA");
   Serial.print("Battery 1 Temperature: "); Serial.print(temperature_batt1); Serial.println(" *C");
   Serial.print("Battery 2 Temperature: "); Serial.print(temperature_batt2); Serial.println(" *C");
   Serial.println();
 
-  delay(100);
+  delay(200);
 }
