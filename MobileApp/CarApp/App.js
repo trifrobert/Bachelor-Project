056fd07a58172a92a581c7590542bcdbe9562cad
Trif Robert <robert.trif@student.upt.ro>
2024-06-03 17:58:36 +0300

    Implementing Push-Notifications using FCM V1.

--- MobileApp/CarApp/App.js
+++ MobileApp/CarApp/App.js
@@ -1,10 +1,110 @@
-import { StyleSheet, View } from 'react-native';
+import 'react-native-gesture-handler';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, Platform, Alert } from 'react-native';
 import Navigation from './src/navigation';
+import { enableScreens } from 'react-native-screens';
+
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
+import { PermissionsAndroid } from 'react-native';
+
+async function requestAndroidPermissions() {
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
+      {
+        title: "Notification Permission",
+        message: "This app needs access to send you notifications",
+        buttonNeutral: "Ask Me Later",
+        buttonNegative: "Cancel",
+        buttonPositive: "OK"
+      }
+    );
+    if (granted !== PermissionsAndroid.RESULTS.GRANTED) {
+      Alert.alert("Permission not granted", "You will not receive notifications");
+    }
+  } catch (err) {
+    console.warn(err);
+  }
+}
+
+enableScreens();
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
+function handleRegistrationError(errorMessage) {
+  Alert.alert("Registration Error", errorMessage);
+  throw new Error(errorMessage);
+}
 
 export default function App() {
+  const [expoPushToken, setExpoPushToken] = useState("");
+
+  useEffect(() => {
+    console.log("Registering for push notifications...");
+    registerForPushNotificationsAsync()
+      .then((token) => {
+        console.log("token: ", token);
+        setExpoPushToken(token);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  async function registerForPushNotificationsAsync() {
+    let token;
+
+    if (Platform.OS === "android") {
+      await requestAndroidPermissions();
+      await Notifications.setNotificationChannelAsync("default", {
+        name: "default",
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: "#FF231F7C",
+      });
+    }
+
+    if (Device.isDevice) {
+      const { status: existingStatus } =
+        await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== "granted") {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== "granted") {
+        Alert.alert("Failed to get push token", "Notification permission not granted!");
+        return;
+      }
+
+      const projectId = "cd95f3fa-91c8-4571-80e4-60234eb9ddac";
+      if (!projectId) {
+        handleRegistrationError('Project ID not found');
+      }
+      try {
+        token = (
+          await Notifications.getExpoPushTokenAsync({
+            projectId,
+          })
+        ).data;
+        console.log(token);
+      } catch (e) {
+        handleRegistrationError(`${e}`);
+      }
+    } else {
+      Alert.alert("Physical Device Required", "Must use physical device for Push Notifications");
+    }
+    return token;
+  }
+
   return (
     <View style={styles.container}>
-      <Navigation/>
+      <Navigation />
     </View>
   );
 }
@@ -12,5 +112,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  }
+  },
 });
